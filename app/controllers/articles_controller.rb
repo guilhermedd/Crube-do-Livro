3076638d5399ba2ff3969402daf239653c29a0e6
Guilherme Diel <guilherme@pop-os.localdomain>
2021-03-18 20:36:39 -0300

    Ta bonitinho agora

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,47 +1,48 @@
 class ArticlesController < ApplicationController
     def index
-        @articles = Article.all
+        @livros = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
+        @livro = Article.find(params[:id])
     end
 
     def new
     end
 
     def edit
-        @article = Article.find(params[:id])
+        @livro = Article.find(params[:id])
     end
 
     def create
-        @article = Article.new(article_params)
+        @livro = Article.new(article_params)
 
-        if @article.save
-        redirect_to @article
+        if @livro.save
+        redirect_to @livro
         else
             render 'new'
         end
     end
 
     def update
-        @article = Article.find(params[:id])
+        @livro = Article.find(params[:id])
 
-        if @article.update(article_params)
-            redirect_to @article 
+        if @livro.update(article_params)
+            redirect_to @livro 
         else
             render 'edit'
         end
     end
 
     def destroy
-        @article = Article.find(params[:id])
-        @article.destroy
+        @livro = Article.find(params[:id])
+        @livro.destroy
 
         redirect_to articles_path
     end
 
     private
+    
     def article_params
         params.require(:article).permit(:title, :text, :pub)
     end
